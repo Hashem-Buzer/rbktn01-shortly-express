92b4062071db1f6918e97682e87054e99756a760
hashem-buzer <hbuzer98@gmail.com>
2019-12-21 00:02:02 +0100

    c2

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,21 @@
+const model = require('../models')
+
 const parseCookies = (req, res, next) => {
+  if(req.headers.cookie){
+    let parsedCookie = {};
+    console.log(req.headers.cookie)
+    console.log(req)
+    req.headers.cookie.split(' ').forEach(cookie => {
+      let splited = cookie.split('=');
+      parsedCookie[splited[0]] = splited[1];
+    });
+    req.cookies = parsedCookie;
+
+    // res.cookies = parsedCookie;
+    next(req, res);
+  } else {
+    res.status(401).send();
+  }
 };
 
 module.exports = parseCookies;
