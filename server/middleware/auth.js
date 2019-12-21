ecf7f3b1cbf89ef16de5943248b809f943916b64
hashem-buzer <hbuzer98@gmail.com>
2019-12-21 17:07:30 +0100

    c4

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,29 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  Promise.resolve(req.cookies.shortlyId)
+  .then(hash=> {
+    if (!hash) {
+      throw hash;
+    }
+    return models.Sessions.get({hash})
+  })
+  .then(session=> {
+    if (!session) {
+      throw session;
+    }
+    return session
+  })
+  .catch(()=> {
+    return models.Sessions.create()
+    .then(results=> {
+      return models.Sessions.get({id: results.insertId});
+    })
+  })
+  .then(session=> {
+    req.session = session;
+    next();
+  })
 };
 
 /************************************************************/
